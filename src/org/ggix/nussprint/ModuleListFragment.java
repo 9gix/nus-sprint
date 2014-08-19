28c58927a2d8b0cc2195a161892f600bd9cdce21
Eugene <yeo.eugene.oey@gmail.com>
2014-08-19 13:06:59 +0800

    Linking between module list with the items

---
+++ src/org/ggix/nussprint/ModuleListFragment.java
@@ -0,0 +1,19 @@
+package org.ggix.nussprint;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+public class ModuleListFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.mod_list, container, false);
+		ListView lv = (ListView) v.findViewById(R.id.module_list_view);
+		lv.setAdapter(new ModuleListAdapter(getActivity()));
+		return v;
+	}
+}
